0ded5411c5ec92c1b83c391661d41010a45f87ab
maks <maxim_86nva@mail.ru>
2023-05-14 18:47:51 +0300

    созданы классы Book и Author с полями, конструкторами, геттерами и сеттерами

---
+++ src/com/hw/maxim/Author.java
@@ -0,0 +1,21 @@
+package com.hw.maxim;
+
+public class Author {
+
+    private String name;
+
+    private String secondName;
+
+    public Author(String name, String secondName) {
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -3,6 +3,20 @@ package com.hw.maxim;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        Author alexanderPushkin = new Author("Александр", "Пушкин");
+        Book ruslanILydmilya = new Book("Руслан и Людмила", alexanderPushkin, 1820);
+        Book kapitanskayaDochka = new Book("Капитанская дочка", alexanderPushkin, 1823);
+
+        System.out.println(String.format("%s написана автором %s %s в %s году", ruslanILydmilya.getName(), ruslanILydmilya.getAuthor().getName(),ruslanILydmilya.getAuthor().getSecondName(), ruslanILydmilya.getYear()));
+        System.out.println(String.format("%s написана автором %s %s в %s году", kapitanskayaDochka.getName(), kapitanskayaDochka.getAuthor().getName(),kapitanskayaDochka.getAuthor().getSecondName(), kapitanskayaDochka.getYear()));
+
+        Author mikhailBulgakov = new Author("Михаил", "Булгаков");
+        Book sobacheSerdce = new Book("Собачье сердце", mikhailBulgakov, 1924);
+        Book masterIMargarita = new Book("Мастер и Маргарита", mikhailBulgakov, 1925);
+        System.out.println(String.format("%s написана автором %s %s в %s году", sobacheSerdce.getName(), sobacheSerdce.getAuthor().getName(),sobacheSerdce.getAuthor().getSecondName(), sobacheSerdce.getYear()));
+
+        sobacheSerdce.setYear(0);
+
+        System.out.println(String.format("%s написана автором %s %s в %s году", sobacheSerdce.getName(), sobacheSerdce.getAuthor().getName(),sobacheSerdce.getAuthor().getSecondName(), sobacheSerdce.getYear()));
     }
 }
